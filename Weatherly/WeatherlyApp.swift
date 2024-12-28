2b9c4205f75ac3f5f28f0c440cc14aedb2bc83ad
kalinduagathisi <kalinduagathisi@gmail.com>
2024-12-28 11:27:53 +0530

    initial commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Kalindu Agathisi on 2024-12-28.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
